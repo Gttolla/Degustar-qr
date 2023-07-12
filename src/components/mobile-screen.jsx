08a074bc6d882b0df67bf25acabc672e12945502
Gttolla <lucaspedrosupergeeks@gmail.com>
2023-07-12 00:20:59 -0300

    React/Tailwind cleaning and config, React RouterDOM, Netlify deploy, MobileScreen component and props

---
+++ src/components/mobile-screen.jsx
@@ -0,0 +1,12 @@
+
+function MobileScren (props){
+    return(
+        <div className="container">
+            <div className="mobile">
+                <div className="screen">{props.content}</div>
+            </div>
+        </div>
+    );
+}
+
+export default MobileScren;
---
+++ src/pages/main/main.jsx
@@ -0,0 +1,12 @@
+import MobileScren from "../../components/mobile-screen";
+
+function Main (){
+    const content =
+        <div>content</div>
+
+    return(
+        <MobileScren content={content} />
+    );
+}
+
+export default Main;
