c71266cafccd919f8439293f322fb9bd7dc08937
Gttolla <lucaspedrosupergeeks@gmail.com>
2023-07-24 18:38:59 -0300

    React-icons install

--- src/components/contact-bar.jsx
+++ src/components/contact-bar.jsx
@@ -1,8 +1,15 @@
+import { FaWhatsapp as Wp } from 'react-icons/fa6'
+import { BiEnvelope as Email} from 'react-icons/bi'
+
 function ContactBar (){
     return(
         <header className="bg-primary p-6">
-            <nav>
-                <span className="text-text font-medium font-titles">Seja bem vindo!</span>
+            <nav className="flex flex-row items-center">
+
+                <span className="text-text font-medium font-titles basis-4/5">Seja bem vindo!</span>
+                
+                <Wp className="ContactBar-icon" size={25} color="white"/>
+                <Email className="ContactBar-icon" size={30} color="white"/>
 
             </nav>
         </header>
