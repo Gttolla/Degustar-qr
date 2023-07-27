b38c5d91d2e1b6142cf6fe6b750ac53c65312e36
Gttolla <lucaspedrosupergeeks@gmail.com>
2023-07-27 19:46:33 -0300

    Readme update

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 [Start application](https://degustarbuffet-socialmedias.netlify.app/)
 
+[Figma model](https://www.figma.com/file/y8LusJB8okxn6YFqTGIeWf/Degustar-Buffet?type=design&mode=design&t=toRrzhZccvW8Yfvc-1)
+
--- src/pages/main/main.jsx
+++ src/pages/main/main.jsx
@@ -1,7 +1,9 @@
 import MobileScren from "../../components/mobile-screen";
-import Img from "../../assets/imgs/img.jpg"
 import TopScreen from "../../components/top-screen";
 import ContactBar from "../../components/contact-bar";
+
+import Img from "../../assets/imgs/img.jpg"
+import Logo from "../../assets/imgs/logo.jpg"
 
 function Main (){
 
@@ -13,8 +15,23 @@ function Main (){
             </div>
 
             <div className="h-[396px] w-full relative">
-                <div className="w-full h-2/4" id="background"></div>
+
+                <div className="w-full h-1/2" id="gradient">
+                    <div className="flex items-center mt-4">
+                        <div className="h-24 w-24 p-2 mr-2 bg-white rounded-full border-[3px] border-primary">
+                            <img src={Logo} alt="logo" className="h-full w-full rounded-full"/>
+                        </div>
+
+                        <div className="leading-5">
+                            <span className="text-black font-titles text-[20px]">Degustar Buffet</span>
+                            <br />
+                            <span className="">Eventos empresariais</span>
+                        </div>
+                    </div>
+                </div>
+
                 <img src={Img} alt="img" className="w-full h-full" />
+            
             </div>
         </div>
         
